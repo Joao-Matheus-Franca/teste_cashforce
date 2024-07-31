205e049b440125e70865ae9f66182ec538dd48b5
João Matheus Silva Franca <matheussfranca@live.com>
2024-07-31 15:47:19 -0300

    refactor: corrigir coluna da tabela cnpjs

--- back-end/src/migrations/20240731183501-cnpj-migration.js
+++ back-end/src/migrations/20240731183501-cnpj-migration.js
@@ -10,7 +10,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER(11)
       },
-      name: {
+      cnpj: {
         allowNull: false,
         type: Sequelize.STRING(255)
       },
