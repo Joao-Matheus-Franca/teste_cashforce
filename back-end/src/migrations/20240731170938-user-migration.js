b9e083f7e10421b723a1c5ed0c4a7026b0323236
João Matheus Silva Franca <matheussfranca@live.com>
2024-07-31 14:32:04 -0300

    feat: adicionar Migration para usuários com Sequelize

---
+++ back-end/src/migrations/20240731170938-user-migration.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER(11)
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(255)
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(255)
+      },
+      phoneNumber: {
+        type: Sequelize.STRING(255)
+      },
+      mobile: {
+        type: Sequelize.STRING(255)
+      },
+      departament: {
+        type: Sequelize.STRING(255)
+      },
+      verificationCode: {
+        type: Sequelize.STRING(255)
+      },
+      emailChecked: {
+        type: Sequelize.TINYINT(1)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      cashforceAdm: {
+        type: Sequelize.TINYINT(1)
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
