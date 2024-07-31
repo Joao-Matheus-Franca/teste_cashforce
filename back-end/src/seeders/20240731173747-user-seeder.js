2acb85bfb9e76bb2cd390434716bb642f6e9bfbc
João Matheus Silva Franca <matheussfranca@live.com>
2024-07-31 16:36:05 -0300

    feat: popular tabela de cnpjs com o Seeder do Sequelize

--- back-end/src/seeders/20240731173747-user-seeder.js
+++ back-end/src/seeders/20240731173747-user-seeder.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    queryInterface.bulkInsert('users', 
+    await queryInterface.bulkInsert('users', 
       [
         {
           id: 1,
