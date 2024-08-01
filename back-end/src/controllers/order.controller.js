756c292abe21780e1330fc8a967224d6860c3685
João Matheus Silva Franca <matheussfranca@live.com>
2024-08-01 11:14:52 -0300

    feat: adicionar camada Controller da entidade Order

---
+++ back-end/src/controllers/order.controller.js
@@ -0,0 +1,13 @@
+const { orderService } = require('../services/order.service')
+
+const getAll = async (_req, res) => {
+    try {
+      const orders = await orderService.getAll();
+      return res.status(200).json(orders);
+    } catch (e) {
+      console.log(e.message);
+      res.status(500).json({ message: 'Erro ao carregar pedidos' });
+    }
+  };
+  
+  module.exports = getAll;
